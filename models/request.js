2bfac4bb2d891390fe7665f4ce99ac8bd9ecf0ef
Tian Xie <tian@animoto.com>
2014-12-10 13:40:49 -0500

    Added polling and templates

--- models/request.js
+++ models/request.js
@@ -3,6 +3,7 @@ var Url = require('url');
 var path = require('path');
 var http = require('http');
 var https = require('https');
+var ejs = require('ejs');
 
 var _ = require('underscore');
 var Async = require('async');
@@ -15,15 +16,76 @@ var Request = function(opts, requestLoader, logger) {
 
   self.requestLoader = requestLoader;
 
-  self.load = requestLoader.loadFunction(opts);
-
+  self._loader = requestLoader.loadFunction(opts);
   self.parser = opts.parser;
-
   self.opts = opts;
-
   self.logger = logger;
 
   return self;
+};
+
+Request.prototype.loadReq = function(req) {
+  this.request = req;
+  this.url = Url.parse(req.url);
+};
+
+Request.prototype.loadObj = function(obj) {
+  this.metadata = obj.metadata;
+  this.parser = obj.parser;
+
+  this.loadReq(obj.request);
+
+  this.success = this.successFunction(obj.success);
+  this.poll = this.pollFunction(obj.poll);
+
+  if (obj.prereq) {
+    this.prereq = new Request(obj.prereq, this.requestLoader, this.logger);
+  }
+};
+
+Request.prototype.loadFunction = function() {
+  var self = this;
+
+  return function load(cb) {
+    self._loader(function(error, obj) {
+      if (error) {
+        cb(error);
+      } else {
+        self.loadObj(obj);
+        cb();
+      }
+    });
+  };
+};
+
+Request.prototype.conditionFunction = function(conditionStr) {
+  var self = this;
+
+  return function customCondition() {
+    return "1" === ejs.render(conditionStr, { 'locals': {'responseBody' : self.responseBody, 'response' : self.response}});
+  };
+};
+
+Request.prototype.successFunction = function(successStr) {
+  var self = this;
+
+  if (successStr) {
+    return self.conditionFunction(successStr);
+  } else {
+    // Default behavior for success is statusCode 20[0-9]
+    return self.conditionFunction("<% if (/20[0-9]/.exec(response.statusCode)) { %>1<% } else { %>0<% } %>");
+  }
+};
+
+Request.prototype.pollFunction = function(pollStr) {
+  var self = this;
+
+  if (pollStr) {
+    return self.conditionFunction(pollStr)
+  } else {
+    // Default behavior for polling is no polling
+    return function() { return false; }
+  }
 };
 
 Request.prototype.requestHeaders = function() {
@@ -48,24 +110,6 @@ Request.prototype.requestBody = function() {
   }
 };
 
-Request.prototype.loadObjFunction = function() {
-  var self = this;
-
-  return function loadObj(obj, cb) {
-    self.logger.debug("Loading request", obj);
-    
-    self.metadata = obj.metadata;
-    self.parser = obj.parser;
-    self.request = obj.request;
-
-    if (obj.prereq) {
-      self.prereq = new Request(obj.prereq, self.requestLoader, self.logger);
-    }
-
-    cb();
-  };
-};
-
 Request.prototype.runPrereqFunction = function() {
   var self = this;
 
@@ -81,28 +125,15 @@ Request.prototype.runPrereqFunction = function() {
 Request.prototype.applyPrereqFunction = function() {
   var self = this;
 
-  return function(responseBody, repsonse, cb) {
+  return function(cb) {
     if (self.prereq) {
-      var requestStr = JSON.stringify(self.request);
-
-      requestStr = requestStr.replace(/#{response(Body)?\.([^}]*)}/, function(fullMatch, bodyMatch, keysMatch) {
-        var result = utils.deepValue((bodyMatch ? responseBody : response), keysMatch.split('.'));
-        self.logger.debug("Replacing '"+fullMatch+"' with '"+result+"'", self.metadata);
-        return result;
-      });
-
-      requestStr = requestStr.replace(/#{request\.([^}]*)}/, function(fullMatch, keysMatch) {
-        var result = utils.deepValue(self.prereq.request, keysMatch.split('.'));
-        self.logger.debug("Replacing '"+fullMatch+"' with '"+result+"'", self.metadata);
-        return result;
-      });
+      requestStr = ejs.render(JSON.stringify(self.request), {'locals' : { 'prereq' : self.prereq }});
 
       utils.parseJSON(requestStr, function(error, obj) {
-        self.request = obj;
+        self.loadReq(obj);
         cb(error);
       });
     } else {
-      cb = responseBody;
       cb();
     }
   };
@@ -114,7 +145,7 @@ Request.prototype.sendRequestFunction = function() {
   return function sendReq(cb) {
     self.logger.debug("Sending request: "+self.request.method+" '"+self.request.url+"'", self.metadata);
 
-    var url = Url.parse(self.request.url);
+    var url = self.url;
 
     var protocol = (/^https/.exec(url.protocol) ? https : http);
 
@@ -137,42 +168,82 @@ Request.prototype.parseBodyFunction = function() {
   var self = this;
 
   return function parseBody(responseBody, response, cb) {
+    var logResponse = function(body, resp) {
+      self.logger.info(
+        "\nSTATUS CODE: "+resp.statusCode+
+        "\nHEADERS: "+_.reduce(resp.headers, function(memo, val, key) { return memo + '\n  '+key+': '+val; }, '')+
+        "\nBODY:\n"+body
+      );
+    };
+
     if (/application\/.*json/.exec(response.headers['content-type']) || self.parser === "json") {
       utils.parseJSON(responseBody, function(error, obj) {
-        var responseBodyStr = JSON.stringify(obj, undefined, 2);
-        self.logger.info(
-          "\nSTATUS CODE: "+response.statusCode+
-          "\nHEADERS: "+_.reduce(response.headers, function(memo, val, key) { return memo + '\n  '+key+': '+val; }, '')+
-          "\nBODY:\n"+responseBodyStr
-        );
-
-        cb(error, obj, response);
+        if (error) {
+          cb(error);
+        } else {
+          var responseBodyStr = JSON.stringify(obj, undefined, 2);
+          logResponse(responseBodyStr, response);
+          cb(null, obj, response)
+        }
       });
     } else {
       // noop
-      self.logger.info(
-        "\nSTATUS CODE: "+response.statusCode+
-        "\nHEADERS: "+_.reduce(response.headers, function(memo, val, key) { return memo + '\n  '+key+': '+val; }, '')+
-        "\nBODY:\n"+responseBody
-      );
-      cb(null, responseBody, response);
+      logResponse(responseBody, response);
+      cb(null, responseBody, response)
     }
+  };
+};
+
+Request.prototype.setResponseFunction = function() {
+  var self = this;
+
+  return function setResponse(responseBody, response, cb) {
+    self.response = response;
+    self.responseBody = responseBody;
+    cb();
+  };
+};
+
+Request.prototype.sendFunction = function() {
+  var self = this;
+
+  return function sendParsePoll(cb) {
+    Async.waterfall(
+      [
+        self.sendRequestFunction(),
+        self.parseBodyFunction(),
+        self.setResponseFunction()
+      ], function poll(error) {
+        if (error) {
+          cb(error);
+        } else {
+          if (self.success()) {
+            // Success condition has been met
+            cb();
+          } else if (self.poll()) {
+            // Poll condition has been met
+            setTimeout(function() {sendParsePoll(cb); }, 1000);
+          } else {
+            cb(new Error("Sending request: "+self.request.method+" '"+self.request.url+"'"));
+          } 
+        }
+      }
+    );
   };
 };
 
 Request.prototype.run = function(runCb) {
   var self = this;
 
-  var tasks = [
-    self.load,
-    self.loadObjFunction(),
-    self.runPrereqFunction(),
-    self.applyPrereqFunction(),
-    self.sendRequestFunction(),
-    self.parseBodyFunction()
-  ];
-
-  Async.waterfall(tasks, runCb);
+  Async.waterfall(
+    [
+      self.loadFunction(),
+      self.runPrereqFunction(),
+      self.applyPrereqFunction(),
+      self.sendFunction(),
+    ],
+    runCb
+  );
 };
 
 module.exports = Request;
